7d2dd8346cae32243eaf5f3be368f090cdf3f1a6
JoshuaRhee <jh.rhee@kriss.re.kr>
2023-01-30 02:01:05 +0900

    ~Lesson4.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
 
 void main() {
   runApp(const TikTocApp());
@@ -14,7 +15,7 @@ class TikTocApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: const Color(0xffe9435a),
       ),
-      home: Container(),
+      home: const SignUpScreen(),
     );
   }
 }
